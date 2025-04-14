64501b8458e5ca2ec8cfece26ec259bcc230f62c
AbeNarimani <AbeNarimani@gmail.com>
2025-04-14 18:08:07 +0330

    death animation added

---
+++ Assets/Scripts/GamePlay/Unit/UnitRagDoll.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+namespace GamePlay.Unit
+{
+    public class UnitRagDoll : MonoBehaviour
+    {
+        [SerializeField] private Transform ragDollRootBone;
+
+        public void SetUp(Transform originalRagDollRootBone)
+        {
+        }
+    }
+}
