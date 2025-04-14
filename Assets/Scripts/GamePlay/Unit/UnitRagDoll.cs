011d9bf50a81845686c459358da63241f6b1075a
AbeNarimani <AbeNarimani@gmail.com>
2025-04-14 18:18:02 +0330

    added some fix

--- Assets/Scripts/GamePlay/Unit/UnitRagDoll.cs
+++ Assets/Scripts/GamePlay/Unit/UnitRagDoll.cs
@@ -8,6 +8,22 @@ namespace GamePlay.Unit
 
         public void SetUp(Transform originalRagDollRootBone)
         {
+            MatchAllChildTransforms(originalRagDollRootBone, ragDollRootBone);
+        }
+
+        private void MatchAllChildTransforms(Transform root, Transform clone)
+        {
+            foreach (Transform child in root)
+            {
+                Transform cloneChild = clone.Find(child.name);
+                if (cloneChild != null)
+                {
+                    cloneChild.position = child.position;
+                    cloneChild.rotation = child.rotation;
+
+                    MatchAllChildTransforms(child, cloneChild);
+                }
+            }
         }
     }
 }
